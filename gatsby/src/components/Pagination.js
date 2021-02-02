b437f47fffed3c058226b18bc2f961124bc34978
Jeremiah <jeremiah.fallin@gmail.com>
2021-02-02 12:55:23 -0800

    second commit responsive

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -25,6 +25,12 @@ const PaginationStyles = styled.div`
       color: var(--grey);
     }
   }
+  @media (max-width: 800px) {
+    .word {
+      display: none;
+    }
+    font-size: 1.4rem;
+  }
 `;
 
 export default function Pagination({
@@ -43,7 +49,10 @@ export default function Pagination({
   return (
     <PaginationStyles>
       <Link disabled={!hasPrevPage} to={`/${base}/${prevPage}`}>
-        ← Prev
+        ←{" "}
+        <span title="Previous Page" className="word">
+          Prev
+        </span>
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
         <Link
@@ -55,7 +64,10 @@ export default function Pagination({
         </Link>
       ))}
       <Link disabled={!hasNextPage} to={`/${base}/${nextPage}`}>
-        Next →
+        <span title="Next Page" className="word">
+          Next
+        </span>{" "}
+        →
       </Link>
     </PaginationStyles>
   );
